0a0698fdb3cfb79a7b4ad084411b29165f383bf8
Christopher Martin <ch.martin@gmail.com>
2013-04-22 14:01:17 -0400

    gen-cert.sh: change storepass to changeit, add some line breaks

--- pki/gen-cert.sh
+++ pki/gen-cert.sh
@@ -1,24 +1,39 @@
+#
+# To generate a new cert for 'client1' signed by 'ca':
+#
+#    ./gen-cert.sh client1
+#
+
 KEYSTORE=$1.jks
-STOREPASS=storepass
+STOREPASS=changeit
 
 ALIAS=$1
 KEYPASS=keypass
 
 CA_KEYPASS=keypass
 
-PKCS12_STOREPASS=storepass
+PKCS12_STOREPASS=changeit
 
 # Generate key pair and store it in $KEYSTORE at alias $ALIAS
-keytool -keystore $KEYSTORE -storepass $STOREPASS -genkeypair -alias $ALIAS -keyalg RSA -dname CN=$1 -keypass keypass
+keytool -keystore $KEYSTORE -storepass $STOREPASS \
+        -genkeypair -alias $ALIAS -keyalg RSA -dname CN=$1 -keypass keypass
 
 # Generate a CSR and use it to generate a cert ($1.pem) signed by CA
-keytool -keystore $KEYSTORE -storepass $STOREPASS -certreq -alias $ALIAS -keypass "$KEYPASS" | keytool -keystore ca.jks -storetype JKS -storepass storepass -gencert -alias ca -keypass "$CA_KEYPASS" -rfc > $1.pem
+keytool -keystore $KEYSTORE -storepass $STOREPASS \
+        -certreq -alias $ALIAS -keypass "$KEYPASS" | \
+keytool -keystore ca.jks -storetype JKS -storepass storepass \
+        -gencert -alias ca -keypass "$CA_KEYPASS" -rfc > $1.pem
 
 # Import the CA cert into $KEYSTORE so that we can import the CSR reply
-keytool -keystore $KEYSTORE -storepass $STOREPASS -importcert -alias ca -file ca.pem -noprompt
+keytool -keystore $KEYSTORE -storepass $STOREPASS \
+        -importcert -alias ca -file ca.pem -noprompt
 
 # Import the CSR reply (the cert issued by CA) into $KEYSTORE
-keytool -keystore $KEYSTORE -storepass $STOREPASS -importcert -alias $ALIAS -keypass "$KEYPASS" -file $1.pem
+keytool -keystore $KEYSTORE -storepass $STOREPASS \
+        -importcert -alias $ALIAS -keypass "$KEYPASS" -file $1.pem
 
 # Export the cert and key pair in the PKCS#12 format to $1.p12
-keytool -importkeystore -srckeystore $KEYSTORE -srcstorepass $STOREPASS -destkeystore $1.p12 -deststoretype PKCS12 -deststorepass "$PKCS12_STOREPASS" -srcalias $ALIAS -srckeypass "$KEYPASS"
+keytool -importkeystore -srckeystore $KEYSTORE -srcstorepass $STOREPASS \
+        -destkeystore $1.p12 -deststoretype PKCS12 -deststorepass "$PKCS12_STOREPASS" \
+        -srcalias $ALIAS -srckeypass "$KEYPASS"
+
